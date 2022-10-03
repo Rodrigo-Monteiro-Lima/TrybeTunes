bbb97c3fdc51c8dceb4d06ead7e4b9ba5a772d92
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-03 18:51:41 -0300

    6 requisito

---
+++ src/components/AlbumSearch.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class AlbumSearch extends Component {
+  render() {
+    const { artist, albums } = this.props;
+    if (albums[0] === 'Nenhum álbum foi encontrado') {
+      return <div>Nenhum álbum foi encontrado</div>;
+    }
+    return (
+      <div>
+        {`Resultado de álbuns de: ${artist}`}
+        {albums
+          .map(({ artistName, collectionName, collectionId, artworkUrl100 }) => (
+            <li
+              key={ collectionId }
+            >
+              {collectionName}
+              <img src={ artworkUrl100 } alt={ collectionName } />
+              {artistName}
+              <Link
+                to={ `/album/${collectionId}` }
+                data-testid={ `link-to-album-${collectionId}` }
+              >
+                Details
+              </Link>
+            </li>))}
+
+      </div>
+    );
+  }
+}
+
+AlbumSearch.propTypes = {
+  artist: PropTypes.string.isRequired,
+  albums: PropTypes.arrayOf(PropTypes.shape({
+    collectionId: PropTypes.number,
+    artistName: PropTypes.string,
+    artworkUrl100: PropTypes.string,
+    collectionName: PropTypes.string,
+  })).isRequired,
+};
+
+export default AlbumSearch;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
+import AlbumSearch from '../components/AlbumSearch';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       search: '',
+      isLoading: false,
+      albums: [],
+      artist: '',
     };
   }
 
@@ -12,11 +18,25 @@ class Search extends Component {
     const { name, value } = target;
     this.setState({
       [name]: value,
+      artist: value,
     });
   };
 
+  handleClick = async () => {
+    const { artist } = this.state;
+    this.setState({
+      isLoading: true,
+      search: '',
+    });
+    const result = await searchAlbumsAPI(artist);
+    this.setState(() => ({
+      isLoading: false,
+      albums: result.length !== 0 ? result : ['Nenhum álbum foi encontrado'],
+    }));
+  };
+
   render() {
-    const { search } = this.state;
+    const { search, isLoading, albums, artist } = this.state;
     const minChar = 2;
     return (
       <div data-testid="page-search">
@@ -35,10 +55,14 @@ class Search extends Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ search.length < minChar }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
         </form>
+        {isLoading && <Loading />}
+        {!isLoading && albums.length !== 0
+        && <AlbumSearch albums={ albums } artist={ artist } /> }
       </div>
     );
   }
