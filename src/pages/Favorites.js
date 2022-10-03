2fcb351254f337a477014c36ad9c8e30708d415c
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-03 14:42:22 -0300

    1 requisito

---
+++ src/pages/Favorites.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        {}
+      </div>
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        {}
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
