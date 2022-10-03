bb4566f9791bc0306dccb10d6b8f1a803d45610d
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-03 15:09:25 -0300

    5 requisito

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+    const minChar = 2;
     return (
       <div data-testid="page-search">
-        {}
+        <form>
+          <label htmlFor="artist">
+            <input
+              type="text"
+              id="artist"
+              data-testid="search-artist-input"
+              name="search"
+              onChange={ this.handleChange }
+              value={ search }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ search.length < minChar }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
